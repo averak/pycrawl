64edfdf39128743a204f09e7450ea4e052cc36eb
AjxLab <abe12@mccc.jp>
2020-01-10 13:37:04 +0900

    add __init__.py

---
+++ pycrawl/__init__.py
@@ -0,0 +1,10 @@
+from .pycrawl import *
+
+__copyright__    = 'Copyright (C) 2019 Tatsuya Abe'
+__version__      = '1.0.0'
+__license__      = 'BSD-3-Clause'
+__author__       = 'Tatsuya Abe'
+__author_email__ = 'abe12@mccc.jp'
+__url__          = 'https://github.com/AjxLab/PyCrawl'
+
+__all__ = ['PyCrawl']
