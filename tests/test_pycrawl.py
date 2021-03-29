732b04c2b10b8cc9fa6a43f0d885484730dd9c8b
averak <abe12@mccc.jp>
2021-03-29 21:48:15 +0900

    [test] find nodes test

--- tests/test_pycrawl.py
+++ tests/test_pycrawl.py
@@ -1,3 +1,4 @@
+import re
 import unittest
 from pycrawl import PyCrawl
 
@@ -21,8 +22,8 @@ class TestAPI(unittest.TestCase):
         node = lxml.html.fromstring(self.sample_html)
         spider = PyCrawl(node=node)
         self.assertEqual(
-            self.sample_html.replace("\n", ""),
-            spider.outer_text().replace("\n", ""),
+            re.sub(r'[\n ]', '', self.sample_html),
+            re.sub(r'[\n ]', '', spider.outer_text())
         )
 
     def test_create_with_html(self):
@@ -30,8 +31,31 @@ class TestAPI(unittest.TestCase):
         self.assertEqual(self.sample_html, spider.html)
 
     def test_send_params(self):
-        spider = PyCrawl(html=self.sample_url)
+        spider = PyCrawl(html=self.sample_html)
         spider.send(id="test id", value="hello")
         spider.send(id="test id", check=True)
         spider.send(id="test id", file_name="tests/sample.html")
         self.assertEqual(3, len(spider.params))
+
+    def test_find_node_with_css(self):
+        spider = PyCrawl(html=self.sample_html)
+        self.assertEqual(6, len(spider.css('p')))
+
+    def test_find_node_with_xpath(self):
+        spider = PyCrawl(html=self.sample_html)
+        self.assertEqual(4, len(spider.xpath("/html/body/p")))
+
+    def test_find_node_with_attr(self):
+        spider = PyCrawl(html=self.sample_html)
+        self.assertEqual('sample text 3', spider.css('#test_id').inner_text())
+        self.assertEqual('sample text 4', spider.css('.test_class').inner_text())
+
+    def test_find_deep_node(self):
+        spider = PyCrawl(html=self.sample_html)
+        self.assertEqual('sample text 5', spider.css('div').css('p').inner_text())
+        self.assertEqual('sample text 5', spider.css('div').css('p')[0].inner_text())
+        self.assertEqual('sample text 6', spider.css('div').css('p')[1].inner_text())
+
+    def test_find_node_attr(self):
+        spider = PyCrawl(html=self.sample_html)
+        self.assertEqual(self.sample_url, spider.css('a').attr("href"))
